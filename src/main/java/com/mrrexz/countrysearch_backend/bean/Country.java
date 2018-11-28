5ba29b91e76fc32c8f30b58253b744ca06205328
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-28 11:48:14 +0700

    Added Country bean

---
+++ src/main/java/com/mrrexz/countrysearch_backend/bean/Country.java
@@ -0,0 +1,32 @@
+package com.mrrexz.countrysearch_backend.bean;
+
+public class Country {
+
+    private String countryName;
+    private double latitude;
+    private double longitude;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
