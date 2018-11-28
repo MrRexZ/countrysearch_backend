55a0c7fb7877fbf8c2418e2725c54a421f56e2b3
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-28 16:30:01 +0700

    Allowed Cross Origin request

--- src/main/java/com/mrrexz/countrysearch_backend/controller/CountryController.java
+++ src/main/java/com/mrrexz/countrysearch_backend/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.mrrexz.countrysearch_backend.controller;
 
 import com.mrrexz.countrysearch_backend.bean.Country;
 import com.mrrexz.countrysearch_backend.service.ICountryService;
+import com.mrrexz.countrysearch_backend.util.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ public class CountryController {
     @Autowired
     ICountryService countryService;
 
+    @CrossOrigin(origins = Config.CLIENT_URL)
     @RequestMapping(value = "getCountries", method = RequestMethod.GET)
     public @ResponseBody List<Country> getMatchingSortedCountries(@RequestParam("search") String countryToSearch) {
         List<Country> matchingSortedCountry = countryService.getMatchingSortedClosestCountries(countryToSearch);
