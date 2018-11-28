2a76c1e2350bcdb8eb035493c96f5b6569af5c31
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-28 11:29:34 +0700

    Init spring backend project

---
+++ src/main/java/com/mrrexz/countrysearch_backend/CountrysearchBackendApplication.java
@@ -0,0 +1,12 @@
+package com.mrrexz.countrysearch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrysearchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountrysearchBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mrrexz/countrysearch_backend/CountrysearchBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mrrexz.countrysearch_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CountrysearchBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
