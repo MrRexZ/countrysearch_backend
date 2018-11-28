ed8edc3da37d4f01b8e35d3f91073318ff9975b5
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-11-28 23:02:22 +0700

    Implemented correct sorting logic

--- src/main/java/com/mrrexz/countrysearch_backend/service/country/filter/CountryFilterService.java
+++ src/main/java/com/mrrexz/countrysearch_backend/service/country/filter/CountryFilterService.java
@@ -43,12 +43,12 @@ public class CountryFilterService implements ICountryFilterService {
         return countriesToSort.stream().sorted((country, anotherCountry) -> {
             double distanceToFirstCountry = locationService.getDistance(serverLatLng.getLatitude(), serverLatLng.getLongitude(), country.getLatitude(), country.getLongitude());
             double distanceToSecondCountry = locationService.getDistance(serverLatLng.getLatitude(), serverLatLng.getLongitude(), anotherCountry.getLatitude(), anotherCountry.getLongitude());
-            if (distanceToFirstCountry - distanceToSecondCountry > 0) {
-                return -1;
-            } else if (distanceToFirstCountry - distanceToSecondCountry == 0) {
+            if (distanceToFirstCountry > distanceToSecondCountry ) {
+                return 1;
+            } else if (distanceToFirstCountry == distanceToSecondCountry ) {
                 return 0;
             } else {
-                return 1;
+                return -1;
             }
         }).collect(Collectors.toList());
 
